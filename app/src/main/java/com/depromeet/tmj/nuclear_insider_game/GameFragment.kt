56b96d079657c0f3f8b8856e758b78ef966feb0a
trycatch <trycatch98@gmail.com>
2018-11-18 06:31:27 +0900

    퀴즈 id, score 점수 수정

--- app/src/main/java/com/depromeet/tmj/nuclear_insider_game/GameFragment.kt
+++ app/src/main/java/com/depromeet/tmj/nuclear_insider_game/GameFragment.kt
@@ -71,14 +71,14 @@ class GameFragment : Fragment(), RewardedVideoAdListener, AnkoLogger {
     }
 
     private fun getQuiz() =
-            api.getQuiz("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30")
+            api.getQuiz(idArray.toString().substring(1, idArray.toString().length - 1))
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .doOnComplete {
                 changeQuiz()
             }
             .doOnError {
-                (activity as MainActivity).gameOver("${currentQuestion + 1}")
+                (activity as MainActivity).gameOver("$currentQuestion")
             }
             .flatMap {
                 if(it.isEmpty())
