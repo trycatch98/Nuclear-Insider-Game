fd19e1fcfc3a5063441cf91e59695c1841e40d4d
Yoo Hyun Sik <dbgustlr92@gmail.com>
2018-11-17 22:27:21 +0900

    스타일 수정

--- app/src/main/java/com/depromeet/tmj/nuclear_insider_game/MainActivity.kt
+++ app/src/main/java/com/depromeet/tmj/nuclear_insider_game/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.depromeet.tmj.nuclear_insider_game
 
+import android.graphics.Paint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUi()
+    }
+
+    private fun initUi() {
+//        tv_team_name.paintFlags += Paint.UNDERLINE_TEXT_FLAG
     }
 }
---
+++ app/src/main/java/com/depromeet/tmj/nuclear_insider_game/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.depromeet.tmj.nuclear_insider_game
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
